ca81e609e041ab3b76893b0a8b5c678ae2853fd3
ahmad hasby assidiqi <echo.ahmadhasyassidiqi@gmail.com>
2023-12-18 14:56:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Sidebar from './components/Sidebar'
+import "./app.css"
+const App = () => {
+  return (
+    <div className='app'> 
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../app.css"
+const Sidebar = () => {
+  return (
+    <div className='Sidebar'>
+        <h1>sinau bareng</h1>
+    </div>
+  )
+}
+
+export default Sidebar
